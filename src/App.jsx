269183648adb417116d223e0bf021892f695a866
Himasnhu <himanshubhartimax@gmail.com>
2024-07-03 01:26:39 +0530

    Ass Features Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features"
 import Hero from "./components/Hero"
 import Higlights from "./components/Higlights"
 import Model from "./components/Model"
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Higlights />
       <Model />
+      <Features />
     </main>
   )
 }
